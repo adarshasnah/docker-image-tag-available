7a68c4cf1aabbf3c169e6e882af952c05b702098
Adarsh Hasnah <adarshasnah@chb-technologies.ch>
2020-10-01 14:48:08 +0400

    change image_name to app_name

--- docker_image_tag_available/cli.py
+++ docker_image_tag_available/cli.py
@@ -6,12 +6,12 @@ import sys
 @click.option("--registry-url", envvar='REGISTRY_URL', prompt="Enter url for docker registry", required=True, help="Url for docker registry")
 @click.option("--user", envvar='REGISTRY_USER', prompt="User", required=True, help="Username for docker registry")
 @click.option("--password", envvar='REGISTRY_PWD', prompt="Password", required=True, help="Password for docker registry")
-@click.option("--image-name", envvar='IMAGE_NAME', prompt="Image", required=True, help="Image for docker registry")
+@click.option("--app-name", envvar='APP_NAME', prompt="Image", required=True, help="Image for docker registry")
 @click.option("--image-version", envvar='IMAGE_VERSION', prompt="Version", required=True, help="Version for docker registry")
-def main(registry_url, user, password, image_name, image_version):    
+def main(registry_url, user, password, app_name, image_version):    
 
     try:        
-        r = requests.get('%s/v2/%s/tags/list' % (registry_url, image_name), auth=(user, password))
+        r = requests.get('%s/v2/%s/tags/list' % (registry_url, app_name), auth=(user, password))
         r.raise_for_status()
     except requests.exceptions.HTTPError:
         click.echo(click.style('Cannot connect to registry', fg='red'))
